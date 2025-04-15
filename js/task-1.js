3c4b9b1f5fa33d1aabdce8bd0e9b5534fbc26e53
Hanna Ponomarenko <pnmrnk.anna@gmail.com>
2025-04-15 21:10:27 +0200

    add new files

---
+++ js/task-1.js
@@ -0,0 +1,30 @@
+
+function isEnoughCapacity(products, containerSize) {
+    let totalFruits = 0;
+    for (let count of Object.values(products)) {
+        totalFruits += count;
+    }
+    return totalFruits <= containerSize;
+}
+
+
+
+
+
+
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
